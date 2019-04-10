dd091751df7b6a21e7f3d83ea565346d2a36a5cb
LuisDFlores <47601890+LuisDFlores@users.noreply.github.com>
2019-04-10 13:00:35 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+int main()
+{
+
+o Insert 100 randomly generated numbers.
+o Print if the Tree is balanced.
+o Print size of tree
+o Print the tree using all three traversals.
+o Delete Half the Tree
+o Print size of Tree
+o Print the tree using all three traversals.
+o Search for a random value in the tree and print the result
+o Clear the tree.
+• ALL OUTPUT SHOULD BE SENT TO A FILE. USE THE FSTREAM
+LIBRARY TO PRINT OUTPUT TO A FILE.
+
+
+o Insert 100 randomly generated numbers.
+o Print if the Tree is balanced.
+o Print size of tree
+o Print the tree using all three traversals.
+o Delete Half the Tree
+o Print size of Tree
+o Print the tree using all three traversals.
+o Search for a random value in the tree and print the result
+o Clear the tree.
+• ALL OUTPUT SHOULD BE SENT TO A FILE. USE THE FSTREAM
+LIBRARY TO PRINT OUTPUT TO A FILE.
+
+
+o Insert 100 randomly generated numbers.
+o Print if the Tree is balanced.
+o Print size of tree
+o Print the tree using all three traversals.
+o Delete Half the Tree
+o Print size of Tree
+o Print the tree using all three traversals.
+o Search for a random value in the tree and print the result
+o Clear the tree.
+• ALL OUTPUT SHOULD BE SENT TO A FILE. USE THE FSTREAM
+LIBRARY TO PRINT OUTPUT TO A FILE.
+
+
+
+
+o Insert 100 randomly generated numbers.
+o Print if the Tree is balanced.
+o Print size of tree
+o Print the tree using all three traversals.
+o Delete Half the Tree
+o Print size of Tree
+o Print the tree using all three traversals.
+o Search for a random value in the tree and print the result
+o Clear the tree.
+• ALL OUTPUT SHOULD BE SENT TO A FILE. USE THE FSTREAM
+LIBRARY TO PRINT OUTPUT TO A FILE.
+return 0;
+}
